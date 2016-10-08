2f8dad1665e7f7110fd76adc8da3b632ff252239
Dennis Schubert <mail@dennis-schubert.de>
2016-10-08 09:38:10 +0200

    Add button to reset prefs

--- _assets/javascripts/about.js
+++ _assets/javascripts/about.js
@@ -7,4 +7,14 @@ document.addEventListener("DOMContentLoaded", function() {
     this.dispatchEvent(event);
     e.preventDefault();
   });
+
+  document.getElementById("delPrefs").addEventListener("click", function(e) {
+    localStorage.clear();
+    document.getElementById("preferences").style.display = "none";
+    e.preventDefault();
+  });
+
+  if (localStorage.length > 0) {
+    document.getElementById("preferences").style.display = "block";
+  }
 });
