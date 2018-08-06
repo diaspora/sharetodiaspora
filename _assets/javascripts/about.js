f4c19b5e09193bf5761aa2a5497c9af9c63232c2
Dennis Schubert <mail@dennis-schubert.de>
2018-08-06 02:43:54 +0200

    Social Providers are no longer a thing in Firefox

--- _assets/javascripts/about.js
+++ _assets/javascripts/about.js
@@ -1,13 +1,6 @@
 "use strict";
 
 document.addEventListener("DOMContentLoaded", function() {
-  document.getElementById("fxAdd").addEventListener("click", function(e) {
-    var event = new CustomEvent("ActivateSocialFeature");
-    this.setAttribute("data-service", JSON.stringify(window.firefoxShareData));
-    this.dispatchEvent(event);
-    e.preventDefault();
-  });
-
   document.getElementById("delPrefs").addEventListener("click", function(e) {
     localStorage.clear();
     document.getElementById("preferences").style.display = "none";
